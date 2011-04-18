aa1429cac474f97c7756c1d919d13ce4c33a58b2
kapil nakhwa <kapilnakhwa@live.com>
2011-04-18 11:43:49 +0545

    Reviews and pointing system modified

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,40 +1,57 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 function externalLinks() {
- if (!document.getElementsByTagName) return;
- var anchors = document.getElementsByTagName("a");
- for (var i=0; i<anchors.length; i++) {
-   var anchor = anchors[i];
-   if (anchor.getAttribute("href") &&
-       anchor.getAttribute("rel") == "external")
-     anchor.target = "_blank";
- }
+  if (!document.getElementsByTagName) return;
+  var anchors = document.getElementsByTagName("a");
+  for (var i=0; i<anchors.length; i++) {
+    var anchor = anchors[i];
+    if (anchor.getAttribute("href") &&
+    anchor.getAttribute("rel") == "external")
+    anchor.target = "_blank";
+  }
 }
 window.onload = externalLinks;
 
 $(function() {
 
-   $("#like, #dislike, #noauth_dislike, #noauth_like").hover(
-      function () {
-        $(this).css("opacity", 1);
-      },
-      function () {
-        $(this).css("opacity", .2);
-      }
-    );
+  $("#like, #dislike, #noauth_dislike, #noauth_like").hover(
+    function () {
+      $(this).css("opacity", 1);
+    },
+    function () {
+      $(this).css("opacity", .2);
+    }
+  );
 
-    $("#response_count").hover(
-        function () {
-            $("#response_count_message").html("Response Count is Total Count of Both <br />Likes & Dislikes").fadeIn("slow");
-        },
-        function () {
-            $("#response_count_message").html("");
-        }
-    );
+  $("#response_count").hover(
+    function () {
+      $("#response_count_message").html("Response Count is Total Count of Both <br />Likes & Dislikes").fadeIn("slow");
+    },
+    function () {
+      $("#response_count_message").html("");
+    }
+  );
 
-    $("#noauth_like a, #noauth_dislike a").click(function(event) {
-        if(!confirm("Login to like or dislike this video?")){
-            event.preventDefault();
-        }
-    });
+  $("#noauth_like a, #noauth_dislike a").click(function(event) {
+    if(!confirm("Login to like or dislike this video?")){
+      event.preventDefault();
+    }
+  });
+
+  $("#review_content").keyup(function () {
+    if($("#review_content").val().length >= 200){
+      $("#review_submit").show();
+      $(".text_value").hide();
+      $("#checkbox").attr('checked',true);
+    }
+    else{
+      $(".text_value").show();
+      var length = $("#review_content").val().length;
+      var remaining = 200 - length;
+      $(".text_value").html(+remaining+" characters left");
+      $("#review_submit").hide();
+      $("#checkbox").attr('checked',false);
+    }
+  });
+
 });
