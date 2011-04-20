d92f08858232f334d7227f293ac09bd86442e148
kapil nakhwa <kapilnakhwa@live.com>
2011-04-20 14:59:50 +0545

    thumbnail path generator changed

--- spec/models/like_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe Like do
-   it { should belong_to :person }
-   it { should belong_to :video }
-   it { should validate_presence_of(:video_id) }
-   it { should validate_presence_of(:person_id) }
-end
